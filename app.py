2779aa650a7a901a42d11413077364b45482f280
Hemant Jain <jainhemant163@gmail.com>
2020-06-27 19:35:05 -0400

    app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
@@ -64,8 +64,8 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
-    #LOG.info(f"prediction: {prediction}")
-    LOG.info("prediction: \n"+prediction)
+    LOG.info(f"prediction: {prediction}")
+    #LOG.info("prediction: \n"+prediction)
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
